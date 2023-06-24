ce9c4bedb46f24bf7f24ee2e87ce1a140ddce96f
RishinR <rishinrnew2020@gmail.com>
2023-06-24 23:24:48 +0530

    added returning all services fucntionality in user.js

---
+++ backend/src/models/Service.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const ServiceSchema = new mongoose.Schema({
+  // service name should be the _id here
+  _id:{
+    type: String, 
+    required: true,
+  },
+  companyId:{
+    type:String, 
+    required : true,
+  },
+  serviceType:{
+    type: String,
+    required: true,
+  },
+  serviceCost:{
+    type:Number, 
+    required: true,
+  },
+  serviceDescription:{
+    type: String,
+    required: false,
+  }
+
+});
+
+module.exports = new mongoose.model('Service', ServiceSchema);
