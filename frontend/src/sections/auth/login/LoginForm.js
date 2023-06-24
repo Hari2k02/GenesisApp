22f082fb81190672b595162782b9109c23b6f609
srjm97 <67820312+srjm97@users.noreply.github.com>
2023-06-25 01:00:47 +0530

    Added user login

--- frontend/src/sections/auth/login/LoginForm.js
+++ frontend/src/sections/auth/login/LoginForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // @mui
-import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
+import { Link, Stack, IconButton, InputAdornment, TextField } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 // components
 import Iconify from '../../../components/iconify';
@@ -12,20 +12,56 @@ export default function LoginForm() {
   const navigate = useNavigate();
 
   const [showPassword, setShowPassword] = useState(false);
+  const [credentials, setCredentials] = useState({ username: '', password: '' });
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleClick = () => {
-    navigate('/dashboard', { replace: true });
+  const handleChange = (e) => {
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const handleClick = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('http://localhost:1337/loginUser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      if (response.ok && data.status === 'ok') {
+        navigate('/dashboard', { replace: true });
+      } else if (data.status === 'error') {
+        setErrorMessage('Invalid username or password');
+      }
+    } catch (error) {
+      console.error('Login failed', error);
+      setErrorMessage('Login failed. Please try again.');
+    }
+
+    setLoading(false);
   };
 
   return (
     <>
       <Stack spacing={3}>
-        <TextField name="email" label="Email address" />
+        <TextField name="username" label="Username" value={credentials.username} onChange={handleChange} />
 
         <TextField
           name="password"
           label="Password"
           type={showPassword ? 'text' : 'password'}
+          value={credentials.password}
+          onChange={handleChange}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
@@ -38,14 +74,23 @@ export default function LoginForm() {
         />
       </Stack>
 
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+
       <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
-        <Checkbox name="remember" label="Remember me" />
+        {/* <Checkbox name="remember" label="Remember me" /> */}
         <Link variant="subtitle2" underline="hover">
           Forgot password?
         </Link>
       </Stack>
 
-      <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
+      <LoadingButton
+        fullWidth
+        size="large"
+        type="submit"
+        variant="contained"
+        loading={loading}
+        onClick={handleClick}
+      >
         Login
       </LoadingButton>
     </>
