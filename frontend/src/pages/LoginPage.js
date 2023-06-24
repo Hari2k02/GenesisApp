156ceceb95777b98ab5e80dd95502eae49d5e88c
Neorace22 <siddarthsajeev2002@gmail.com>
2023-06-24 22:53:48 +0530

    changes to login

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -38,7 +38,6 @@ const StyledContent = styled('div')(({ theme }) => ({
   padding: theme.spacing(12, 0),
 }));
 
-
 // ----------------------------------------------------------------------
 
 export default function LoginPage() {
@@ -47,7 +46,7 @@ export default function LoginPage() {
   return (
     <>
       <Helmet>
-        <title> Login | GradeGo </title>
+        <title> Login | Minimal UI </title>
       </Helmet>
 
       <StyledRoot>
@@ -71,32 +70,32 @@ export default function LoginPage() {
         <Container maxWidth="sm">
           <StyledContent>
             <Typography variant="h4" gutterBottom>
-              Login to GradeGo
+              Sign in to Minimal
             </Typography>
 
-            {/* <Typography variant="body2" sx={{ mb: 5 }}>
+            <Typography variant="body2" sx={{ mb: 5 }}>
               Don’t have an account? {''}
               <Link variant="subtitle2">Get started</Link>
-            </Typography> */}
+            </Typography>
 
-              {/* <Stack direction="row" spacing={2}>
-                <Button fullWidth size="large" color="inherit" variant="outlined">
-                  <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
-                </Button>
+            <Stack direction="row" spacing={2}>
+              <Button fullWidth size="large" color="inherit" variant="outlined">
+                <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
+              </Button>
 
-                <Button fullWidth size="large" color="inherit" variant="outlined">
-                  <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
-                </Button>
+              <Button fullWidth size="large" color="inherit" variant="outlined">
+                <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
+              </Button>
 
-                <Button fullWidth size="large" color="inherit" variant="outlined">
-                  <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
-                </Button>
-              </Stack> */}
+              <Button fullWidth size="large" color="inherit" variant="outlined">
+                <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
+              </Button>
+            </Stack>
 
             <Divider sx={{ my: 3 }}>
-              {/* <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
                 OR
-              </Typography> */}
+              </Typography>
             </Divider>
 
             <LoginForm />
--- frontend/src/sections/auth/login/LoginForm.js
+++ frontend/src/sections/auth/login/LoginForm.js
@@ -8,41 +8,7 @@ import Iconify from '../../../components/iconify';
 
 // ----------------------------------------------------------------------
 
-
 export default function LoginForm() {
-
-  const [ktu_id, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errMsg, setErrMsg] = useState(false);
-  const validateUser = async (event) => {
-    event.preventDefault();
-    const response = await fetch("http://localhost:1337/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        ktu_id,
-        password,
-      }),
-    });
-
-    const data = await response.json();
-    console.log(data);
-    if (data.status === 'ok') {
-      if (data.user === 'student') {
-        window.location.href = '/dashboard'
-      }
-      else if (data.user === 'faculty') {
-        window.location.href = '/facdashboard'
-      }
-    }
-    else {
-      setErrMsg(true);
-      alert("Incorrect username or password")
-    }
-    console.log(data);
-  }
   const navigate = useNavigate();
 
   const [showPassword, setShowPassword] = useState(false);
@@ -54,12 +20,11 @@ export default function LoginForm() {
   return (
     <>
       <Stack spacing={3}>
-        <TextField name="email" onChange={(e) => setUsername(e.target.value)} label="Ktu Id" />
+        <TextField name="email" label="Email address" />
 
         <TextField
           name="password"
           label="Password"
-          onChange={(e) => setPassword(e.target.value)}
           type={showPassword ? 'text' : 'password'}
           InputProps={{
             endAdornment: (
@@ -72,19 +37,15 @@ export default function LoginForm() {
           }}
         />
       </Stack>
-      
-          
-      
-          
+
       <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
         <Checkbox name="remember" label="Remember me" />
-        
         <Link variant="subtitle2" underline="hover">
           Forgot password?
         </Link>
       </Stack>
 
-      <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={validateUser}>
+      <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
         Login
       </LoadingButton>
     </>
