11563eab519481df35a50e3cbec562793342ddaa
Hari2k02 <harivellikkunnam@gmail.com>
2023-06-25 00:58:53 +0530

    model for feedback edited
    feddback api created

--- backend/src/models/Feedback.js
+++ backend/src/models/Feedback.js
@@ -1,23 +1,38 @@
 const mongoose = require('mongoose');
 
 const FeedbackSchema = new mongoose.Schema({
-    //_id is the username of users
+    //_id is the service name
   _id: {
     type: String,
     required: true,
   },
-  postedDate:{
-    type: Date,
-    required: true,
-  },
-  rating:{
-    type: Number,
-    required: true,
-  },
-  review:{
-    type: String,
-    required: false,
-  }
+  feedbacks : [
+    {
+      //_id is the username of users
+      _id:{
+        type: String,
+        required: true,
+      },
+      postedDate:{
+        type: Date,
+        required: true,
+      },
+      rating:{
+        type: Number,
+        required: true,
+      },
+      review:{
+        type: String,
+        required: false,
+      }
+    }
+  ]
+
+
+
+  
 });
 
 module.exports = new mongoose.model('feedback', FeedbackSchema);
+
+
